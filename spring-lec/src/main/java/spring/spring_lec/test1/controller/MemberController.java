cca4b77027e9be034bba48d4f87006dfb299942f
kkb <0furiousblade0@gmail.com>
2024-07-22 17:26:37 +0900

    [#9] Spring 복습 - 회원조회 기능 추가

--- spring-lec/src/main/java/spring/spring_lec/test1/controller/MemberController.java
+++ spring-lec/src/main/java/spring/spring_lec/test1/controller/MemberController.java
@@ -2,10 +2,13 @@ package spring.spring_lec.test1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import spring.spring_lec.test1.domain.Member;
 import spring.spring_lec.test1.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -34,4 +37,12 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping("members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
